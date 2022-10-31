0bef4f25546b5d73d46f96b79d1ac72e07a78f68
Mateusz <wzmateusz@gmail.com>
2022-10-31 14:59:53 +0100

    website

--- src/js/main.js
+++ src/js/main.js
@@ -11,12 +11,11 @@ const body = document.querySelector('body')
 const contactSection = document.querySelector('.contact')
 
 
-
 function activeMenu () {
 	let len = sections.length
-	while(--len && window.scrollY + 94 < sections[len].offsetTop){}
+	while(--len && window.scrollY + 94 < sections[len]?.offsetTop){}
 	navList.forEach(ltx => ltx.classList.remove('action-scroll'))
-	navList[len].classList.add('action-scroll')
+	navList[len]?.classList.add('action-scroll')
 	if (contactSection?.getBoundingClientRect().top <= 94) {
 		navList.forEach(ltx => ltx.classList.remove('action-scroll'))
 		navList[navList.length - 1].classList.add('action-scroll')
@@ -27,19 +26,19 @@ activeMenu()
 
 const handleNav = () => {
 	nav.classList.toggle('active')
-
+	
 	allNavItems.forEach(item => {
 		item.addEventListener('click', () => {
 			nav.classList.remove('active')
 		})
 	})
-
+	
 	handleNavItemsAnimation()
 }
 
 const handleNavItemsAnimation = () => {
 	let delayTime = 0
-
+	
 	allNavItems.forEach(item => {
 		item.classList.toggle('nav-items-animation')
 		item.style.animationDelay = '.' + delayTime + 's'
@@ -57,5 +56,7 @@ ScrollReveal({
 ScrollReveal().reveal(headerText, { delay: 500 })
 ScrollReveal().reveal(midText, { delay: 500, distance: '0px' })
 
+
 navBtn.addEventListener('click', handleNav)
 window.addEventListener('scroll', activeMenu)
+
